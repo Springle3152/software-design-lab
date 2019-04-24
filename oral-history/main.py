86fc417c803b47adf386fa51a47a83a8316ffe58
Christofer Gass <47193690+christofergass02@users.noreply.github.com>
2019-04-23 22:05:11 -0400

    Add files via upload

--- oral-history/main.py
+++ oral-history/main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/about")
+@app.route("/add")
 def about():
-    return render_template("about.html")
+    return render_template("add.html")
 
 @app.route("/archive")
 def archive():
